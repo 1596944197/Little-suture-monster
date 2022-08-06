34c71aced926df1ca82cd56705b31385b0446c40
mei_yiyi <1596944197@qq.com>
2022-08-06 17:07:31 +0800

    setting:增加eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-undef':0,
+		'@typescript-eslint/no-var-requires': 0
+	}
+};

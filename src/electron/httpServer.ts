3827a93da7e25c983c08161c58b3a6f9f5ad543b
mei_yiyi <1596944197@qq.com>
2022-08-21 17:03:28 +0800

    feat:安装koa，测试cookie

--- src/electron/httpServer.ts
+++ src/electron/httpServer.ts
@@ -1,9 +1,12 @@
 import Koa from 'koa';
+import jwt from 'jsonwebtoken';
 
+const token = jwt.sign({ foo: 'foo' }, 'shhh');
 export default () => {
   const app = new Koa();
 
   app.use(async ctx => {
+    ctx.cookies.set('token', token, { overwrite: true });
     ctx.body = '<h2>hello world</h2>';
   });
 

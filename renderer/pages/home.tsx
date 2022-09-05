0d76630a12b68ebb34f0708fec2f9be940d6579a
1596944197 <1596944197@qq.com>
2022-09-05 18:30:55 +0800

    refactor:使用nextectron

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (with-typescript)</title>
+      </Head>
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -
+          <Link href="/next">
+            <a>Go to next page</a>
+          </Link>
+        </p>
+        <img src="/images/logo.png" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
--- src/electron/preload.ts
+++
@@ -1,4 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("version", () => process.versions);
-contextBridge.exposeInMainWorld("ping", async () => await ipcRenderer.invoke("ping"));

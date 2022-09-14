8f7726573cacc7140f0d85b4341320456b75e7f4
1596944197 <1596944197@qq.com>
2022-09-14 14:19:43 +0800

    temp:临时存储

--- main/module/reptile/reptile.ts
+++ main/module/reptile/reptile.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { net } from "electron";
+import puppeteer from "puppeteer";
 
 const url = "https://www.douyin.com/video/7141326473436171551";
+const cookie =
+  "douyin.com; ttwid=1%7CO4m60a5HNDqSES-DKLSRBsv9oBz45D7kRr20-LCeNXI%7C1663122854%7C094c7f9d8eb7e4f0c400c24af3b4f718f2eea679337037c3a3691f3310075df0; douyin.com; strategyABtestKey=1663122856.406; s_v_web_id=verify_l810epi9_FMFsfrRa_AeI7_4ZaU_8Lta_0qfPCOMAoBn5; passport_csrf_token=98638e7a6cff09ef45c237e5646bd2ac; passport_csrf_token_default=98638e7a6cff09ef45c237e5646bd2ac; ttcid=f208e31d5bd543d5914a25e0848df7cb26; n_mh=MUMSnAPppd4UE4O9BEspY8bGd5QelvNwp3SUVxpy1HQ; sso_uid_tt=cb156e2df1cc93aafeecac6c5c79e383; sso_uid_tt_ss=cb156e2df1cc93aafeecac6c5c79e383; toutiao_sso_user=f6cced66219762a8462a01de0c186946; toutiao_sso_user_ss=f6cced66219762a8462a01de0c186946; sid_ucp_sso_v1=1.0.0-KGMxOGU5OWFkNWIwNjM4NmEwMDdlOTk2NjkyNzhkMTU1NTYwY2Q2N2MKHQi8uISpxAIQw_uEmQYY7zEgDDDd_YvTBTgGQPQHGgJsZiIgZjZjY2VkNjYyMTk3NjJhODQ2MmEwMWRlMGMxODY5NDY; ssid_ucp_sso_v1=1.0.0-KGMxOGU5OWFkNWIwNjM4NmEwMDdlOTk2NjkyNzhkMTU1NTYwY2Q2N2MKHQi8uISpxAIQw_uEmQYY7zEgDDDd_YvTBTgGQPQHGgJsZiIgZjZjY2VkNjYyMTk3NjJhODQ2MmEwMWRlMGMxODY5NDY; odin_tt=fc3ffeb9865d738343fa60a9c802934d57190ff3ffafb5a5e4679a1b349311a567611b62dfd18ba43c340a53b6c80cf0; passport_auth_status=f366e086e23886fc848ac7cfffd8f76d%2C; passport_auth_status_ss=f366e086e23886fc848ac7cfffd8f76d%2C; sid_guard=02f4102e9ce0c863fa1acc085d6f828e%7C1663122883%7C5184000%7CSun%2C+13-Nov-2022+02%3A34%3A43+GMT; uid_tt=fea01547c0e251cf1f245aef77cba69d; uid_tt_ss=fea01547c0e251cf1f245aef77cba69d; sid_tt=02f4102e9ce0c863fa1acc085d6f828e; sessionid=02f4102e9ce0c863fa1acc085d6f828e; sessionid_ss=02f4102e9ce0c863fa1acc085d6f828e; sid_ucp_v1=1.0.0-KDE1ZDg5NGVhYWVmM2EyOWUxNTQyODU2OWE1OGEzZDczNmFiY2MwZTEKFwi8uISpxAIQw_uEmQYY7zEgDDgGQPQHGgJsZiIgMDJmNDEwMmU5Y2UwYzg2M2ZhMWFjYzA4NWQ2ZjgyOGU; ssid_ucp_v1=1.0.0-KDE1ZDg5NGVhYWVmM2EyOWUxNTQyODU2OWE1OGEzZDczNmFiY2MwZTEKFwi8uISpxAIQw_uEmQYY7zEgDDgGQPQHGgJsZiIgMDJmNDEwMmU5Y2UwYzg2M2ZhMWFjYzA4NWQ2ZjgyOGU; __ac_nonce=063213dc30086e87e9f12; _tea_utm_cache_2018=undefined; FOLLOW_LIVE_POINT_INFO=%22MS4wLjABAAAADqbkjqzPjd0agTxgKjm0TafDWhI3cP2aTW99Ntl2v5k%2F1663171200000%2F0%2F1663122897412%2F0%22; csrf_session_id=f6c673ff18a7aa29e21ac493d81b5de4; msToken=gSzT1_IfyWG233GPdPs69EI_DmTQuZ6gwN3NRYi5924lbFPQqnuZ_Y_MC8ENXyFC8LXTvJ4Tf8NF0VEc1dVkSGbTakfLKh1_MzTSp4ArKhtOCcKq9lbcFg==; tt_scid=DRfSmmEwZ0Oog98TL5eTSKf2PcN7qVgjZOeVqsV2vb.sabF8tLri9FOOFI4Qo9khabc0; home_can_add_dy_2_desktop=%221%22; msToken=3Zt_DDzdPHxchBbzuJLQ5foOAO954pyLHjwcOyVm4WBr25jzeEExW5guhoy4wQLWkHVXF9WdJWn821oEHdYugYRuEVLoe2ugOW3ONdRea6gA2d_GQub5Fw==; download_guide=%221%2F20220914%22";
 
 const req = async () => {
   return new Promise((resolve, reject) => {
@@ -9,10 +13,7 @@ const req = async () => {
     const request = net.request({
       url,
     });
-    request.setHeader(
-      "cookie",
-      "douyin.com; __ac_referer=https://www.douyin.com/video/7141326473436171551; ttwid=1%7CWOkU_7WQgcouL6T-bbFYKgb-usJlaQ1YcYNPwP2xapo%7C1662863649%7Cb0caea7db42bfe94593f8e372d195d492848165756c57f33e8c80bf244885f77; ttcid=8492571f859a4648884c8358e59a375033; passport_csrf_token=767bb983d247eb98673cc8753e07e56d; passport_csrf_token_default=767bb983d247eb98673cc8753e07e56d; s_v_web_id=verify_l7wq3wmu_9xNcYYmC_lzEe_4puQ_8Pjx_WRywoMZQnByi; n_mh=MUMSnAPppd4UE4O9BEspY8bGd5QelvNwp3SUVxpy1HQ; sso_uid_tt=6f0c489d6f71d06e414ad450b77f9013; sso_uid_tt_ss=6f0c489d6f71d06e414ad450b77f9013; toutiao_sso_user=db465ef17d4c133c4248ebbe035a0006; toutiao_sso_user_ss=db465ef17d4c133c4248ebbe035a0006; _tea_utm_cache_2018=undefined; download_guide=%223%2F20220911%22; FOLLOW_NUMBER_YELLOW_POINT_INFO=%22MS4wLjABAAAADqbkjqzPjd0agTxgKjm0TafDWhI3cP2aTW99Ntl2v5k%2F1662912000000%2F1662883008419%2F1662883007723%2F0%22; VIDEO_FILTER_MEMO_SELECT=%7B%22expireTime%22%3A1663488527099%2C%22type%22%3A0%7D; FOLLOW_LIVE_POINT_INFO=%22MS4wLjABAAAADqbkjqzPjd0agTxgKjm0TafDWhI3cP2aTW99Ntl2v5k%2F1662912000000%2F0%2F1662883727577%2F0%22; sid_ucp_sso_v1=1.0.0-KDFjZDBhNTdjNzBiNmQ1ZmRkMjA5MmEzODg0Yzg5M2VhOTE1ODgyMmYKHQi8uISpxAIQva_2mAYY7zEgDDDd_YvTBTgGQPQHGgJobCIgZGI0NjVlZjE3ZDRjMTMzYzQyNDhlYmJlMDM1YTAwMDY; ssid_ucp_sso_v1=1.0.0-KDFjZDBhNTdjNzBiNmQ1ZmRkMjA5MmEzODg0Yzg5M2VhOTE1ODgyMmYKHQi8uISpxAIQva_2mAYY7zEgDDDd_YvTBTgGQPQHGgJobCIgZGI0NjVlZjE3ZDRjMTMzYzQyNDhlYmJlMDM1YTAwMDY; passport_auth_status=30a367734d482a55e46c02ac6da8d8b5%2Ca30711cf4b1fedce8d3be934700a3e39; passport_auth_status_ss=30a367734d482a55e46c02ac6da8d8b5%2Ca30711cf4b1fedce8d3be934700a3e39; uid_tt=85939d063a17ba819f28da30fee3d8b9; uid_tt_ss=85939d063a17ba819f28da30fee3d8b9; sid_tt=ccc987451e40185aba2fafeb80f5ef5f; sessionid=ccc987451e40185aba2fafeb80f5ef5f; sessionid_ss=ccc987451e40185aba2fafeb80f5ef5f; __ac_signature=_02B4Z6wo00f01sHE-5QAAIDDH7KjMtdplQbB5P8AANN5n0GqAzMO34fEaz2KG-OBZSNGdqQen14C8HL8qOBpUX6YAjYLLJzPJ4C5RYcsXsN0-XSTYUFVmKT-xMf6vT2shHEP-R-M4MteNC2-b0; strategyABtestKey=1662957640.474; sid_guard=ccc987451e40185aba2fafeb80f5ef5f%7C1662957645%7C5110128%7CThu%2C+10-Nov-2022+08%3A09%3A33+GMT; sid_ucp_v1=1.0.0-KDhiYjdkNjExYmUwYjQ2NmE2YjU5MTBlNzE3MGRiY2Y1ZDRkYjNmY2YKFwi8uISpxAIQzfD6mAYY7zEgDDgGQPQHGgJscSIgY2NjOTg3NDUxZTQwMTg1YWJhMmZhZmViODBmNWVmNWY; ssid_ucp_v1=1.0.0-KDhiYjdkNjExYmUwYjQ2NmE2YjU5MTBlNzE3MGRiY2Y1ZDRkYjNmY2YKFwi8uISpxAIQzfD6mAYY7zEgDDgGQPQHGgJscSIgY2NjOTg3NDUxZTQwMTg1YWJhMmZhZmViODBmNWVmNWY; msToken=9ipPk8o9ZXhR5OnnytDg5Ony9ccQ-JUhISX2NtM3REKFVI3hm5U0Mg3Z4cThw3Vk-pvintZhF4WuRxaLLiBQbP60JSVifRABSFMPJUlTrnlhfLHMIrn9FjlhnNrE145gag==; home_can_add_dy_2_desktop=%221%22; tt_scid=DcLsAENYoh6h-JRZmuHSP.fBrW0yj1frsaA-LNY7HJn9CXhf0oNMwL51WX0Jsh4k2a57; msToken=ffPlJJVbEbjWpHqR3OglzBjlZLG-o2cj1qSlnOE_AhsqKtS5c2n9E3ElvOnr4T5-AYjTmxwjcj04KDLRjDHso45HOVhhG2ePFPmxqjV2MfXS6A9wfqXmQAtk_KIXuRNw; __ac_nonce=0631ef2d80089f41279bf; odin_tt=cbc2d17d7c915415f7e55d3a8dc8091428926feb2ec3bc41a8fd8b40025cb80966c0e86d52ba105f7f2e1841b87952bd; passport_fe_beating_status=false"
-    );
+    request.setHeader("cookie", cookie);
     request.setHeader(
       "user-agent",
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
@@ -50,5 +51,16 @@ const req = async () => {
   });
 };
 
-const reqAll = async () => {};
+const reqAll = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.douyin.com/user/MS4wLjABAAAAshurg6jJDz_utAtWyvfZVVkDQ_EvxYcnoRK_3iUcNDE?relation=0&vid=7143028940628708623"
+  );
+  const aList = await page.$$("li > a");
+  const srcList = await Promise.all(
+    aList.map(async (a) => a.getProperty("href"))
+  );
+  console.log(srcList);
+};
 export { reqAll };
